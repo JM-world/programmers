2a7b1ce9d6a31641f8de14319a89e7eae9ada9c9
Jongmoon Jeong <unhoiylaw@naver.com>
2023-09-05 18:35:17 +0900

    study

---
+++ 코딩_기초_트레이닝/세_개의_구문자.py
@@ -0,0 +1,9 @@
+myStr = "ggggbaconlettucetomato"
+import re
+b = []
+myStr = myStr.replace('a', ' ')
+myStr = myStr.replace('b', ' ')
+myStr = myStr.replace('c', ' ')
+myStr = myStr.strip()
+a = myStr.split(' ')
+print(a)
