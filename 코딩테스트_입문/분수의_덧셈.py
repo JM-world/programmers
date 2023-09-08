3024ad7552a17114f6a4bba07e8b18e3d3685d1f
Jongmoon Jeong <unhoiylaw@naver.com>
2023-09-08 20:10:17 +0900

    study

---
+++ 코딩테스트_입문/분수의_덧셈.py
@@ -0,0 +1,7 @@
+numer1, denom1, numer2, denom2 = 1, 2, 3, 4
+
+if denom1 < denom2:
+    if denom2 % denom1 == 0:
+        num = denom2 / denom1
+        new_numer = numer1 * num + numer2
+        if new_numer 
