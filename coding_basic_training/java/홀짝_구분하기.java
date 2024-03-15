929fc6691dabbeaa13737341d2ed5301dd65ffd1
linxiangkaiho <unhoiylaw@naver.com>
2024-03-15 19:19:00 +0900

    cm2

--- coding_basic_training/java/홀짝_구분하기.java
+++ coding_basic_training/java/홀짝_구분하기.java
@@ -1,7 +1,3 @@
-<<<<<<<< HEAD:coding_basic_training/홀짝_구분하기.java
-package 코딩_기초_트레이닝;
-========
->>>>>>>> d903be9 (programmers):coding_basic_training/java/홀짝_구분하기.java
 
 // import java.util.Scanner;
 public class 홀짝_구분하기 {
