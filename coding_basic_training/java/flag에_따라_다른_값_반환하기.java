b9d24fe8fbe66850332f0ab0e6e6fe0590c25162
linxiangkaiho <unhoiylaw@naver.com>
2024-03-16 10:43:00 +0900

    cm3

--- coding_basic_training/java/flag에_따라_다른_값_반환하기.java
+++ coding_basic_training/java/flag에_따라_다른_값_반환하기.java
@@ -1,7 +1,3 @@
-<<<<<<<< HEAD:coding_basic_training/flag에_따라_다른_값_반환하기.java
-package 코딩_기초_트레이닝;
-========
->>>>>>>> d903be9 (programmers):coding_basic_training/java/flag에_따라_다른_값_반환하기.java
 class flag에_따라_다른_값_반환하기{}
 class Solution {
     public int solution(int a, int b, boolean flag) {
