707dc1e700634cd102533e573d8ba68480db3634
linxiangkaiho <unhoiylaw@naver.com>
2024-04-10 21:11:21 +0900

    te

---
+++ coding_basic_training/java/옷가게_할인_받기.java
@@ -0,0 +1,9 @@
+public class 옷가게_할인_받기 {
+    public int solution(int price) {
+        if (price >= 500000) return (int)(price * 0.8);
+        else if (price >= 300000) return (int)(price * 0.9);
+        else if (price >= 100000) return (int)(price * 0.95);
+        else return price;
+        
+    }
+}
